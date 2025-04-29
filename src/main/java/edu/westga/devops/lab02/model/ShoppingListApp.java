23bd26ded8d3a6731a15362f3ba27fe0f53264c1
Hallie Gearhart <hg00075@my.westga.edu>
2025-04-29 18:37:10 -0400

    Updated model class
    Added model class and test for initialization to complete coverage.

--- src/main/java/edu/westga/devops/lab02/model/ShoppingListApp.java
+++ src/main/java/edu/westga/devops/lab02/model/ShoppingListApp.java
@@ -1,36 +1,18 @@
 package edu.westga.devops.lab02.view;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-
-public class ShoppingListApp extends Application {
+public class ShoppingListApp {
 
     private MainWindow mainWindow;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/westga/devops/lab02/view/MainWindow.fxml"));
-        Parent root = loader.load();
-
-        mainWindow = loader.getController();
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Hallie Gearhart- Shopping List App");
-        primaryStage.show();
+    public ShoppingListApp() {
+        this.mainWindow = new MainWindow();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        ShoppingListApp app = new ShoppingListApp();
     }
-
-    public void saveShoppingList() {
-    }
-
-    public void addItemToList(String item, int quantity) {
-        mainWindow.handleAddItem(item, quantity);
+	
+	public MainWindow getMainWindow() {
+		return mainWindow;
     }
 }
