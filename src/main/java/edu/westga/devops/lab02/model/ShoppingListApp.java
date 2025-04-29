63fa3bc88262461aa5f3538255d9496513b627d3
Hallie Gearhart <hg00075@my.westga.edu>
2025-04-29 18:28:56 -0400

    Fixed test issues
    Completed coverage for view code.

--- src/main/java/edu/westga/devops/lab02/model/ShoppingListApp.java
+++ src/main/java/edu/westga/devops/lab02/model/ShoppingListApp.java
@@ -1,10 +1,36 @@
-package edu.westga.devops.lab02.model;
+package edu.westga.devops.lab02.view;
 
-/** 
- * 
- * @author	Hallie Gearhart
- * @version Spring 2025
- */
-public class ShoppingListApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
 
+public class ShoppingListApp extends Application {
+
+    private MainWindow mainWindow;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/westga/devops/lab02/view/MainWindow.fxml"));
+        Parent root = loader.load();
+
+        mainWindow = loader.getController();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hallie Gearhart- Shopping List App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void saveShoppingList() {
+    }
+
+    public void addItemToList(String item, int quantity) {
+        mainWindow.handleAddItem(item, quantity);
+    }
 }
